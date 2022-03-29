372c82a887c9fcdb6734ff15786c185cef585612
brosio-lsn <ambroise.aigueperse@gmail.com>
2022-03-29 08:37:22 +0200

    test rapidité

--- test/ch/epfl/javelo/routing/RouteComputerTest.java
+++ test/ch/epfl/javelo/routing/RouteComputerTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RouteComputerTest {
     public static void main(String[] args) throws IOException {
-        Path filePath = Path.of("lausanne/nodes_osmid.bin");
+        /*Path filePath = Path.of("lausanne/nodes_osmid.bin");
         LongBuffer osmIdBuffer=null;
         try (FileChannel channel = FileChannel.open(filePath)) {
             osmIdBuffer = channel
@@ -31,7 +31,28 @@ class RouteComputerTest {
         Route r = rc.bestRouteBetween(159049, 117669);
         KmlPrinter.write("javelo.kml", r);
         Route rN= rc.bestRouteBetween(0, 2);
-        System.out.println(rN.edges());
+        System.out.println(rN.edges());*/
+        Path filePath = Path.of("ch_west/nodes_osmid.bin");
+        LongBuffer osmIdBuffer=null;
+        try (FileChannel channel = FileChannel.open(filePath)) {
+            osmIdBuffer = channel
+                    .map(FileChannel.MapMode.READ_ONLY, 0, channel.size())
+                    .asLongBuffer();
+        }
+        catch (Exception e ){
+            System.out.print("rip");
+        }
+        System.out.println(osmIdBuffer.get(50));
+        System.out.println(osmIdBuffer.get(1020));
+
+        Graph g = Graph.loadFrom(Path.of("ch_west"));
+        CostFunction cf = new CityBikeCF(g);
+        RouteComputer rc = new RouteComputer(g, cf);
+        Route r = rc.bestRouteBetween(2046055, 2694240);
+        long t0 = System.nanoTime();
+        KmlPrinter.write("javelo.kml", r);
+        System.out.printf("Itinéraire calculé en %d ms\n",
+                (System.nanoTime() - t0) / 1_000_000);
     }
 
 }
