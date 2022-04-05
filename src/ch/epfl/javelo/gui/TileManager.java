7c34b8c5134270a66fe463c3151797a943adef25
louisrch <louis.roche@epfl.ch>
2022-04-05 09:45:10 +0200

    tilemanager

--- src/ch/epfl/javelo/gui/TileManager.java
+++ src/ch/epfl/javelo/gui/TileManager.java
@@ -1,13 +1,14 @@
 package ch.epfl.javelo.gui;
 
-import java.awt.*;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
+import javafx.scene.image.Image;
+
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 
 public final class TileManager {
@@ -15,10 +16,14 @@ public final class TileManager {
     public final record TileId(int zoomLevel, int xIndex, int yIndex) {
         public boolean isValid(int zoomLevel, int xIndex, int yIndex) {
             double nbOfTiles = Math.scalb(4, zoomLevel);
-            return (xIndex +1 <= Math.sqrt(nbOfTiles) && yIndex +1 <= Math.sqrt(nbOfTiles));
+            return (xIndex >= 0
+                    && yIndex >= 0
+                    && xIndex + 1 <= Math.sqrt(nbOfTiles)
+                    && yIndex + 1 <= Math.sqrt(nbOfTiles));
         }
     }
-    LinkedHashMap<Image, TileId> cache;
+
+    LinkedHashMap<TileId, Image> cache;
     private final Path pathToRepertory;
     private final String serverName;
     private final StringBuilder url;
@@ -27,25 +32,39 @@ public final class TileManager {
         this.pathToRepertory = pathToRepertory;
         this.serverName = serverName;
         url = new StringBuilder();
-        cache = new LinkedHashMap<>(100, 0.75f, false);
+        cache = new LinkedHashMap<>(100, 0.75f, true);
     }
-    public Image imageForTileAt(TileId id) throws IOException {
 
-        url.append("https://tile.openstreetmap.org/")
+    public Image imageForTileAt(TileId id) throws IOException {
+        String fileOfTile = url
                 .append(id.zoomLevel)
                 .append("/")
                 .append(id.xIndex)
                 .append("/")
                 .append(id.yIndex)
-                .append(".png");
-        URL u = new URL(url.toString());
-        URLConnection c = u.openConnection();
-        c.setRequestProperty("User-Agent", "JaVelo");
-        try(InputStream i = c.getInputStream()){
-
+                .append(".png").
+                toString();
+        Path pathOfTile = Path.of(pathToRepertory.toString() + fileOfTile);
+        if (cache.containsKey(id)) {
+            return cache.get(id);
+        } else if (Files.exists(pathOfTile, LinkOption.NOFOLLOW_LINKS)) {
+            Image fileImage = new Image(pathOfTile.toString());
+            cache.put(id, fileImage);
+            return fileImage;
+        } else {
+            Files.createDirectories(pathOfTile);
+            URL u = new URL(serverName + fileOfTile);
+            URLConnection c = u.openConnection();
+            c.setRequestProperty("User-Agent", "JaVelo");
+            try (InputStream i = c.getInputStream()) {
+                OutputStream writer = new FileOutputStream(pathOfTile.toString());
+                i.transferTo(writer);
+            }
+            Image fileImage = new Image(pathOfTile.toString());
+            cache.put(id, fileImage);
+            Iterator<TileId> ite = cache.keySet().iterator();
+            if (ite.hasNext()) cache.remove(ite.next());
+            return fileImage;
         }
-        //TODO ?????????????? ne peut pas utiliser removeEldestEntry ?????
-        //cache.removeEldestEntry;
-
-    return null;}
+        }
 }
