efc12d4cf71d943f0ab237bcc594c3c7b6fbc09f
louisrch <louis.roche@epfl.ch>
2022-04-20 12:32:54 +0200

    baseMapManager ? marche pas encore tt le temps

--- src/ch/epfl/javelo/gui/BaseMapManager.java
+++ src/ch/epfl/javelo/gui/BaseMapManager.java
@@ -1,10 +1,22 @@
 package ch.epfl.javelo.gui;
 
+import ch.epfl.javelo.Math2;
 import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.effect.Light;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
@@ -17,7 +29,10 @@ public final class BaseMapManager {
     private final Canvas canvas;
     private final Pane pane;
     boolean redrawNeeded;
+    EventHandler<MouseEvent> mouseDrag, mouseClick;
+    EventHandler<ScrollEvent> mouseScroll;
     private final static int PIXELS_PER_TILE = 256;
+    private int xTopLeft, yTopLeft, zoomLevel;
 
     public BaseMapManager(TileManager tileManager, WaypointsManager waypointsManager, ObjectProperty<MapViewParameters> property) {
         this.waypointsManager = waypointsManager;
@@ -31,8 +46,12 @@ public final class BaseMapManager {
         });
         redrawOnNextPulse();
         canvas.widthProperty().bind(pane.widthProperty());
+        //  canvas.widthProperty().addListener(property.addListener(redrawOnNextPulse());
+        //});
         canvas.heightProperty().bind(pane.heightProperty());
-        double a = canvas.getWidth();
+        createHandlers();
+        canvas.heightProperty().addListener((observable, oldValue, newValue) -> redrawOnNextPulse());
+        canvas.widthProperty().addListener((o, oV, nV) -> redrawOnNextPulse());
     }
 
     public Pane pane() {
@@ -40,18 +59,18 @@ public final class BaseMapManager {
     }
 
     private void redrawIfNeeded() {
+        xTopLeft = (int) property.getValue().topLeft().getX();
+        yTopLeft = (int) property.getValue().topLeft().getY();
+        zoomLevel = property.getValue().zoomLevel();
         if (!redrawNeeded) return;
         redrawNeeded = false;
-        int xTopLeft = (int) property.getValue().topLeft().getX();
-        int yTopLeft = (int) property.getValue().topLeft().getY();
-        int zoomLevel = property.getValue().zoomLevel();
         GraphicsContext context = canvas.getGraphicsContext2D();
         Image imageToDraw;
         boolean canDraw = true;
-        for (int x = 0; x <= canvas.getWidth()+PIXELS_PER_TILE; x += PIXELS_PER_TILE) {
-            for (int y = 0; y <= canvas.getHeight()+PIXELS_PER_TILE; y += PIXELS_PER_TILE) {
-                int yTileIndex = (yTopLeft+y)/ PIXELS_PER_TILE;
-                int xTileIndex = (xTopLeft+x)/ PIXELS_PER_TILE;
+        for (int x = 0; x <= canvas.getWidth() + PIXELS_PER_TILE; x += PIXELS_PER_TILE) {
+            for (int y = 0; y <= canvas.getHeight() + PIXELS_PER_TILE; y += PIXELS_PER_TILE) {
+                int yTileIndex = (yTopLeft + y) / PIXELS_PER_TILE;
+                int xTileIndex = (xTopLeft + x) / PIXELS_PER_TILE;
                 try {
                     imageToDraw = tileManager.imageForTileAt(new TileManager.TileId(zoomLevel, xTileIndex, yTileIndex));
                 } catch (IOException e) {
@@ -60,12 +79,11 @@ public final class BaseMapManager {
                 }
                 if (canDraw) {
                     System.out.println(xTileIndex + "-" + yTileIndex);
-                    context.drawImage(imageToDraw, xTileIndex*PIXELS_PER_TILE - xTopLeft, yTileIndex*PIXELS_PER_TILE-yTopLeft);
+                    context.drawImage(imageToDraw, xTileIndex * PIXELS_PER_TILE - xTopLeft, yTileIndex * PIXELS_PER_TILE - yTopLeft);
                 }
                 canDraw = true;
             }
         }
-        redrawOnNextPulse();
     }
 
     private void redrawOnNextPulse() {
@@ -73,4 +91,37 @@ public final class BaseMapManager {
         Platform.requestNextPulse();
     }
 
+    private void createHandlers() {
+        mouseScroll = new EventHandler<>() {
+            @Override
+            public void handle(ScrollEvent event) {
+                pane.setOnScroll(this);
+                zoomLevel = (int) Math2.clamp(8, zoomLevel + event.getDeltaY(), 19);
+
+            }
+        };
+        mouseDrag = new EventHandler<>() {
+            @Override
+            public void handle(MouseEvent event) {
+                pane.setOnMouseDragged(this);
+                pane.setOnMouseReleased(this);
+                SimpleObjectProperty<Point2D> mousePoint = new SimpleObjectProperty<>(new Point2D(event.getX(), event.getY()));
+                if (!event.isStillSincePress()) {
+                    double deltaX = event.getX() - mousePoint.get().getX();
+                    double deltaY = event.getY() - mousePoint.get().getY();
+                    mousePoint.get().add(deltaX, deltaY);
+                    xTopLeft += deltaX;
+                    yTopLeft += deltaY;
+                }
+            }
+        };
+        mouseClick = new EventHandler<>() {
+            @Override
+            public void handle(MouseEvent event) {
+                pane.setOnMousePressed(this);
+                waypointsManager.addWayPoint(event.getX(), event.getY());
+
+            }
+        };
+    }
 }
