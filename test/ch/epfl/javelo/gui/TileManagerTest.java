8951cc869cb2b7ab05fb333712f5cf01c12e562c
louisrch <louis.roche@epfl.ch>
2022-05-15 21:21:21 +0200

    y'a des routes en plus :) + meilleur alignement du texte dans ElevationProfileManager

--- test/ch/epfl/javelo/gui/TileManagerTest.java
+++ test/ch/epfl/javelo/gui/TileManagerTest.java
@@ -19,7 +19,7 @@ public class TileManagerTest extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         TileManager tm = new TileManager(
-                Path.of("."), "tile.openstreetmap.org");
+                Path.of("."), "tile.openstreetmap.org", "");
         Image tileImage = tm.imageForTileAt(
                 new TileManager.TileId(19, 271725, 185422));
         Platform.exit();
