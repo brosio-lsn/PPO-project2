a1836a3f706beb946c5d8568f7703b40e3e94101
brosio-lsn <ambroise.aigueperse@gmail.com>
2022-05-27 13:06:22 +0200

    commit

--- src/ch/epfl/javelo/data/GraphEdges.java
+++ src/ch/epfl/javelo/data/GraphEdges.java
@@ -104,11 +104,9 @@ public record GraphEdges(ByteBuffer edgesBuffer, IntBuffer profileIds, ShortBuff
                 }
                 break;
             case COMPRESSED_44:
-                samples[0] = Q28_4.asFloat(elevations.get(profileId));
                 createSamples(nbOfSamples, SAMPLES_PER_SHORT_44, profileId, samples);
                 break;
             case COMPRESSED_04:
-                samples[0] = Q28_4.asFloat(elevations.get(profileId));
                 createSamples(nbOfSamples, SAMPLES_PER_SHORT_04, profileId, samples);
         }
         if (isInverted(edgeId)) {
@@ -160,7 +158,7 @@ public record GraphEdges(ByteBuffer edgesBuffer, IntBuffer profileIds, ShortBuff
      */
     private void createSamples(int nbOfSamples, int samplesPerShort, int profileId, float[] samples) {
         int count = 1;
-        samples[0] = Q28_4.asFloat(elevations.get(profileId));
+        samples[0] = Q28_4.asFloat(Short.toUnsignedInt(elevations.get(profileId)));
         //the number of shorts a compressed profile takes is the number of samples (-1 because
         //the first sample is not compressed) divided by the
         //number of samples a byte can contain, and rounded up since there might exist a byte
